8296168dbb6d1623ebc78fb980445ce4dcfce2b3
PhillipHenry <PhillHenry@gmail.com>
2023-11-21 14:04:00 +0000

    Writes to a bytestream.

--- src/main/java/uk/co/odinconsultants/arrow/ArrowSmokeTest.java
+++ src/main/java/uk/co/odinconsultants/arrow/ArrowSmokeTest.java
@@ -5,8 +5,11 @@ import org.apache.arrow.vector.BitVector;
 import org.apache.arrow.vector.VarCharVector;
 import org.apache.arrow.vector.FieldVector;
 import org.apache.arrow.vector.VectorSchemaRoot;
+import org.apache.arrow.vector.ipc.ArrowStreamWriter;
 import org.apache.arrow.vector.types.pojo.Field;
 
+import java.io.ByteArrayOutputStream;
+import java.nio.channels.Channels;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
@@ -20,9 +23,8 @@ public class ArrowSmokeTest {
      * 	at org.apache.arrow.memory.BaseAllocator.buffer(BaseAllocator.java:310)
      */
     private static final int MAX_ALLOCATION = 1024 * 1024;
-//    private static final int MAX_ALLOCATION = 1024;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try (final RootAllocator allocator =
                      new RootAllocator(MAX_ALLOCATION)) {
             BitVector bitVector = new BitVector("boolean", allocator);
@@ -38,6 +40,34 @@ public class ArrowSmokeTest {
             List<Field> fields = Arrays.asList(bitVector.getField(), varCharVector.getField());
             List<FieldVector> vectors = Arrays.asList(bitVector, varCharVector);
             VectorSchemaRoot root = new VectorSchemaRoot(fields, vectors);
+
+            try (
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    ArrowStreamWriter writer = new ArrowStreamWriter(root, /*DictionaryProvider=*/null, Channels.newChannel(out));
+            ) {
+                // ... do write into the ArrowStreamWriter
+                writer.start();
+// write the first batch
+                writer.writeBatch();
+
+// write another four batches.
+                for (int i = 0; i < 4; i++) {
+                    // populate VectorSchemaRoot data and write the second batch
+                    BitVector childVector1 = (BitVector)root.getVector(0);
+                    VarCharVector childVector2 = (VarCharVector)root.getVector(1);
+                    childVector1.reset();
+                    childVector2.reset();
+                    // ... do some populate work here, could be different for each batch
+                    writer.writeBatch();
+                }
+
+                writer.end();
+            }
+
+            // Else we get:
+            // Exception in thread "main" java.lang.IllegalStateException: Memory was leaked by query. Memory leaked: (17472)
+            //Allocator(ROOT) 0/17472/17504/1048576 (res/actual/peak/limit)
+            // upon JVM finishing
             root.close();
         }
     }
