01be556ea40a3b826b1c4622e61baa2e5cef2520
PhillipHenry <PhillHenry@gmail.com>
2023-11-22 09:54:30 +0000

    Imports

--- src/main/java/uk/co/odinconsultants/arrow/ArrowReadMain.java
+++ src/main/java/uk/co/odinconsultants/arrow/ArrowReadMain.java
@@ -3,24 +3,11 @@ package uk.co.odinconsultants.arrow;
 import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.vector.BitVector;
 import org.apache.arrow.vector.VarCharVector;
-import org.apache.arrow.vector.FieldVector;
 import org.apache.arrow.vector.VectorSchemaRoot;
-import org.apache.arrow.vector.dictionary.Dictionary;
-import org.apache.arrow.vector.dictionary.DictionaryEncoder;
 import org.apache.arrow.vector.ipc.ArrowStreamReader;
-import org.apache.arrow.vector.ipc.ArrowStreamWriter;
-import org.apache.arrow.vector.types.pojo.DictionaryEncoding;
-import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.arrow.vector.types.pojo.Schema;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.nio.channels.Channels;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 import static uk.co.odinconsultants.arrow.ArrowWriteMain.MAX_ALLOCATION;
 import static uk.co.odinconsultants.arrow.ArrowWriteMain.SHM_FILE;
